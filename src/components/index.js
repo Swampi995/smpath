fbc7850a40f472e7e0b24e8c6b020d61b78eae46
Swampi995 <swp.mrs@gmail.com>
2023-10-31 14:48:34 +0200

    Add deletion

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Hero from './hero';
 import Navbar from './Navbar';
 import Contact from './Contact';
+import Delete from './Delete';
 
-export { Hero, Navbar, Contact };
+export { Hero, Navbar, Contact, Delete };
