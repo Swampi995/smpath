e30508688c570ff288456294061956608c1693c9
Swampi995 <swp.mrs@gmail.com>
2024-04-16 12:34:14 +0300

    Add dance cloud

---
+++ src/app/dacecloud/delete/page.jsx
@@ -0,0 +1,12 @@
+'use client';
+import { Delete } from '../../../components';
+
+export default function RaceStatsDelete() {
+  return (
+    <>
+      <div className="relative z-0">
+        <Delete />
+      </div>
+    </>
+  );
+}
---
+++ src/app/dacecloud/policy/page.jsx
@@ -0,0 +1,62 @@
+'use client';
+
+export default function Privacy() {
+  return (
+    <>
+      <div className="relative z-0">
+        <div>
+
+        </div>
+      </div>
+
+      <div className="relative z-0">
+        <h1>Privacy Policy</h1>
+
+        <h2>1. Introduction</h2>
+        <p>Welcome to the &quot;Race Stats&quot; mobile application. This Privacy Policy is designed to help you understand how we collect, use, disclose, and safeguard your personal information when you use our app.</p>
+
+        <h2>2. Information We Collect</h2>
+        <p><strong>A. Location Data</strong><br />We collect and use your location data to provide GPS-based speed tracking. This information is used solely for the purpose of displaying your real-time speed and route information while you use the app.</p>
+
+        <p><strong>B. Firebase</strong><br />When you use the Race Stats app, Firebase collects certain data, including user analytics and crash reports, to help us improve the app&apos;s functionality and performance.</p>
+
+        <p><strong>C. Facebook Login</strong><br />If you choose to log in using your Facebook account, we may collect your public Facebook profile information, email address, and user ID. We use this information for user authentication and account management.</p>
+
+        <p><strong>D. Google Login</strong><br />If you choose to log in using your Google account, we may collect your Google account information, including your email address, profile picture, and user ID. This information is used for user authentication and account management.</p>
+
+        <h2>3. How We Use Your Information</h2>
+        <p>We use the collected information for the following purposes:</p>
+        <ul>
+          <li>To provide you with GPS-based speed tracking services.</li>
+          <li>To improve and personalize your experience with the Race Stats app.</li>
+          <li>To facilitate user authentication through Facebook Login and Google Login.</li>
+          <li>To send you important notifications related to your account or app updates.</li>
+        </ul>
+
+        <h2>4. Data Sharing and Disclosure</h2>
+        <p>We may share your information in the following situations:</p>
+        <ul>
+          <li>With third-party service providers, such as Firebase, to help us improve the app&apos;s functionality and performance.</li>
+          <li>With social media platforms (Facebook and Google) for user authentication purposes.</li>
+          <li>As required by law or to protect our rights, privacy, safety, or property.</li>
+        </ul>
+
+        <h2>5. Data Security</h2>
+        <p>We take reasonable measures to protect your data, but no method of transmission over the internet or electronic storage is completely secure. While we strive to protect your data, we cannot guarantee its absolute security.</p>
+
+        <h2>6. Your Choices</h2>
+        <p>You can choose not to provide certain information, but this may limit your ability to use specific features of the Race Stats app.</p>
+
+        <h2>7. Changes to this Privacy Policy</h2>
+        <p>We may update this Privacy Policy from time to time. Any changes will be posted within the app, and the revised policy will be effective immediately. We encourage you to review this policy periodically.</p>
+
+        <h2>8. Contact Us</h2>
+        <p>If you have any questions, concerns, or requests regarding your personal information or this Privacy Policy, please contact us at [Your Contact Information].</p>
+
+        <h2>9. Acceptance of Terms</h2>
+        <p>By using the Race Stats mobile app, you agree to this Privacy Policy and the practices described herein.</p>
+
+      </div>
+    </>
+  );
+}
