5e404c4abfc3da38c4f6126a97e57fe6b4c2977c
Swampi995 <swp.mrs@gmail.com>
2024-05-01 21:11:21 +0300

    Add apple-app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    experimental: {
+        headers() {
+            return [
+                {
+                    source: "/.well-known/apple-app-site-association",
+                    headers: [{ key: "content-type", value: "application/json" }]
+                }
+            ];
+        }
+    }
+};
