ad852a8a425d6addd8ab2c11d94b5e42f7a6d19e
Swampi995 <swp.mrs@gmail.com>
2024-05-01 21:13:12 +0300

    Fix dancecloud

--- src/app/dancecloud/policy/page.jsx
+++ src/app/dancecloud/policy/page.jsx
@@ -13,12 +13,12 @@ export default function Privacy() {
         <h1>Privacy Policy</h1>
 
         <h2>1. Introduction</h2>
-        <p>Welcome to the &quot;Race Stats&quot; mobile application. This Privacy Policy is designed to help you understand how we collect, use, disclose, and safeguard your personal information when you use our app.</p>
+        <p>Welcome to the &quot;Dance Cloud&quot; mobile application. This Privacy Policy is designed to help you understand how we collect, use, disclose, and safeguard your personal information when you use our app.</p>
 
         <h2>2. Information We Collect</h2>
         <p><strong>A. Location Data</strong><br />We collect and use your location data to provide GPS-based speed tracking. This information is used solely for the purpose of displaying your real-time speed and route information while you use the app.</p>
 
-        <p><strong>B. Firebase</strong><br />When you use the Race Stats app, Firebase collects certain data, including user analytics and crash reports, to help us improve the app&apos;s functionality and performance.</p>
+        <p><strong>B. Firebase</strong><br />When you use the Dance Cloud app, Firebase collects certain data, including user analytics and crash reports, to help us improve the app&apos;s functionality and performance.</p>
 
         <p><strong>C. Facebook Login</strong><br />If you choose to log in using your Facebook account, we may collect your public Facebook profile information, email address, and user ID. We use this information for user authentication and account management.</p>
 
@@ -28,7 +28,7 @@ export default function Privacy() {
         <p>We use the collected information for the following purposes:</p>
         <ul>
           <li>To provide you with GPS-based speed tracking services.</li>
-          <li>To improve and personalize your experience with the Race Stats app.</li>
+          <li>To improve and personalize your experience with the Dance Cloud app.</li>
           <li>To facilitate user authentication through Facebook Login and Google Login.</li>
           <li>To send you important notifications related to your account or app updates.</li>
         </ul>
@@ -45,7 +45,7 @@ export default function Privacy() {
         <p>We take reasonable measures to protect your data, but no method of transmission over the internet or electronic storage is completely secure. While we strive to protect your data, we cannot guarantee its absolute security.</p>
 
         <h2>6. Your Choices</h2>
-        <p>You can choose not to provide certain information, but this may limit your ability to use specific features of the Race Stats app.</p>
+        <p>You can choose not to provide certain information, but this may limit your ability to use specific features of the Dance Cloud app.</p>
 
         <h2>7. Changes to this Privacy Policy</h2>
         <p>We may update this Privacy Policy from time to time. Any changes will be posted within the app, and the revised policy will be effective immediately. We encourage you to review this policy periodically.</p>
@@ -54,7 +54,7 @@ export default function Privacy() {
         <p>If you have any questions, concerns, or requests regarding your personal information or this Privacy Policy, please contact us at [Your Contact Information].</p>
 
         <h2>9. Acceptance of Terms</h2>
-        <p>By using the Race Stats mobile app, you agree to this Privacy Policy and the practices described herein.</p>
+        <p>By using the Dance Cloud mobile app, you agree to this Privacy Policy and the practices described herein.</p>
 
       </div>
     </>
